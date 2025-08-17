3719e6f2edc745f00a707f80d2b257e52e24d80e
Rifky <s22010240@ousl.lk>
2025-08-17 12:16:21 +0530

    after correct cross orgin

--- api/index.js
+++ api/index.js
@@ -16,7 +16,10 @@ const app = express(); // <-- app must be defined first
 const __dirname = path.resolve();
 
 // Middleware
-app.use(cors()); // Enable CORS for all origins
+app.use(cors({
+  origin: '*', // Or replace with: 'https://styledmaven.vercel.app'
+  credentials: true
+})); // Enable CORS for all origins
 app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 
@@ -33,11 +36,11 @@ app.use("/api/post", postRoutes);
 app.use("/api/comment", commentRoutes);
 app.use("/api/job", JobRoutes);
 
-// Serve frontend
-app.use(express.static(path.join(__dirname, '/client/dist')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-});
+// // Serve frontend
+// app.use(express.static(path.join(__dirname, '/client/dist')));
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+// });
 
 // Error handler
 app.use((err, req, res, next) => {
@@ -51,7 +54,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start server
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
