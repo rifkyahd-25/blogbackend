6bb018b4d485e84dcd9df203e3f7a950427410f3
Rifky <s22010240@ousl.lk>
2025-08-17 14:01:31 +0530

    check 3

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -60,6 +60,8 @@ export const signin = async (req, res, next) => {
       .cookie('access_token', token, {
         httpOnly: true,
         sameSite: "none",
+        secure: true ,
+        maxAge: 7 * 24 * 60 * 60 * 1000,
       })
       .json(rest);
   } catch (error) {
